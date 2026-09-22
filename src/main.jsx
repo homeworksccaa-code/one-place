0ef3f7e96a2bbd13929047defa0d5311ed2db22b
homeworksccaa-code <homeworksccaa@gmail.com>
2026-09-21 22:48:49 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,3 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "../Home";
 import "./index.css";
+
+console.log("ONEPLACE: main.jsx carregado");
+console.log("ONEPLACE: Home =", Home);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+);
